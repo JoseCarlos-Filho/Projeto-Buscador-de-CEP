31344eeb87d9c8d22cb80eedc7373ccb98fa88be
José Carlos <etijcvivi@gmail.com>
2022-04-05 20:53:10 -0300

    FACTOR style on project Buscador de CEP

--- src/App.js
+++ src/App.js
@@ -4,22 +4,27 @@ import './estilos/style.css';
 function App() {
   return (
     <div className="container">
-      <h1 className="title">Buscador de CEP</h1>
-      
-      <input type="text" placeholder="Digite seu CEP..." />
+      <div className="Box">
+        <h1 className="title">Buscador CEP</h1>
+        
+        <div className="InputBox">
+          <input type="text" placeholder="Digite seu CEP..." />
 
-      <button type="button" placeholder="buttonSearch">
-        <FiSearch size={25} color="#fff" />
-      </button>
+          <button type="button" placeholder="buttonSearch">
+              <FiSearch size={25} color="#fff" />
+          </button>
+        </div>
+        
 
-      <main className="main">
-          <h2>CEP: 000.00-000</h2>
+        <main className="main">
+            <h2>CEP: 000.00-000</h2>
 
-          <span>Rua: Teste</span>
-          <span>Logradouro: Teste</span>
-          <span>Bairro: Teste</span>
-          <span>Cidade: Teste</span>
-      </main>
+            <span>Rua: Teste</span>
+            <span>Logradouro: Teste</span>
+            <span>Bairro: Teste</span>
+            <span>Cidade: Teste</span>
+        </main>
+        </div>
     </div>
   );
 }
