477a6d1aa732491d8931e98e988fcf02964f3c92
José Carlos <etijcvivi@gmail.com>
2022-04-05 16:59:23 -0300

    Projeto Buscador de Endereço

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import { FiSearch } from "react-icons/fi";
+import './estilos/style.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1 className="title">Buscador de CEP</h1>
+      
+      <input type="text" placeholder="Digite seu CEP..." />
+
+      <button type="button" placeholder="buttonSearch">
+        <FiSearch size={25} color="#fff" />
+      </button>
+
+      <main className="main">
+          <h2>CEP: 000.00-000</h2>
+
+          <span>Rua: Teste</span>
+          <span>Logradouro: Teste</span>
+          <span>Bairro: Teste</span>
+          <span>Cidade: Teste</span>
+      </main>
     </div>
   );
 }
